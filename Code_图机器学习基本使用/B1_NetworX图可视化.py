a8dd30852f52a14d7dc0bc6cdf7cf161460fe95a
Chenling Jia <xxjsjcl@qq.com>
2024-02-27 16:53:24 +0800

    update

--- Code_图机器学习基本使用/B1_NetworX图可视化.py
+++ Code_图机器学习基本使用/B1_NetworX图可视化.py
@@ -2,15 +2,9 @@
 ## 导入工具包
 import networkx as nx
 print("图工具包_networkx:" + nx.__version__)
-import torch
-print("神经网络工具包_pythrch:" + torch.__version__)
 import matplotlib as mpl
 print("数据可视化工具包_matplotlib:" + mpl.__version__)
 import matplotlib.pyplot as plt
-import numpy as np
-print("数学函数和矩阵运算工具包_numpy:" + np.__version__)
-import pandas as pd
-print("数据分析和导入工具包_pandas:" + pd.__version__)
 ## 配置windows 显示中文
 plt.rcParams['font.sans-serif']=['SimHei'] # 用来显示中文标签
 plt.rcParams['axes.unicode_minus']=False  # 用来正常显示正负号
---
+++ Code_图机器学习基本使用/B2_例子_美国128城市交通连接图.py
@@ -0,0 +1,10 @@
+# 配置环境
+## 导入工具包
+import networkx as nx
+print("图工具包_networkx:" + nx.__version__)
+import matplotlib as mpl
+print("数据可视化工具包_matplotlib:" + mpl.__version__)
+import matplotlib.pyplot as plt
+## 配置windows 显示中文
+plt.rcParams['font.sans-serif']=['SimHei'] # 用来显示中文标签
+plt.rcParams['axes.unicode_minus']=False  # 用来正常显示正负号
